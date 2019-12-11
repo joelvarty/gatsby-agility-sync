2518b814b04d7297341022f9b8deac16ef4b9c1d
Joel Varty <joelvarty@outlook.com>
2019-12-11 18:34:23 -0500

    updated page component

--- src/templates/AgilityPage.js
+++ src/templates/AgilityPage.js
@@ -11,25 +11,25 @@ import GlobalHeader from '../components/GlobalHeader.js'
 
 export default class AgilityPage extends Component {
     render() {
-        //HACK
-        // //get the page object from context
-        const page = this.props.pageContext;//.page;
-        // console.log("page", page)
-        // //get the page template name that we need to render
-        // const pageTemplateName = page.templateName.replace(/[^0-9a-zA-Z]/g, '');
 
-        // //build the props
-        // const propsForPageTemplate = {
-        //     pageContext: this.props.pageContext,
-        //     modules: modules
-        // }
-        // const PageTemplateComponentToRender = pageTemplates[pageTemplateName];
+        //get the page object from context
+        const page = this.props.pageContext;//.page;
+        console.log("page", page)
+        //get the page template name that we need to render
+        const pageTemplateName = page.templateName.replace(/[^0-9a-zA-Z]/g, '');
+
+        //build the props
+        const propsForPageTemplate = {
+            pageContext: this.props.pageContext,
+            modules: modules
+        }
+        const PageTemplateComponentToRender = pageTemplates[pageTemplateName];
 
         return (
             <div id="inner-body">
                 <code>{JSON.stringify(page)}</code>
-                {/* <GlobalHeader />
-                <PageTemplateComponentToRender {...propsForPageTemplate} /> */}
+                <GlobalHeader />
+                <PageTemplateComponentToRender {...propsForPageTemplate} />
             </div>
         );
     }
